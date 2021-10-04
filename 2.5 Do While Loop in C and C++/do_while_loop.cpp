305515b7c6225e6fb9dfd23e5b082cf11b4829fd
Kashyap <kashyapshahare03@gmail.com>
2021-10-04 11:37:36 +0530

    C++ Code

---
+++ 2.5 Do While Loop in C and C++/do_while_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int i = 5;
+
+    do
+    {
+        cout << "King" << endl;
+        i--;
+    } while (i>0);
+    
+// This programm will compile once and then check the condition.
+    
+    
+}

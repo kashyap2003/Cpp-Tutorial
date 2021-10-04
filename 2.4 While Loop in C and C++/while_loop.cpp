097ae84ceb56d9221b0c986a73589acf0ccae11b
Kashyap <kashyapshahare03@gmail.com>
2021-10-04 11:37:12 +0530

    C++ Code

---
+++ 2.4 While Loop in C and C++/while_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int i = 5;
+
+    while (i>0)
+    {
+        cout << "Kashyap" << endl;
+        i--;
+    }
+    // Or you can write i-- as i = i-1 also.
+}

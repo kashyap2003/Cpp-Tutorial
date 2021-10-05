7aa2f9cb3304711e5624b9dcb71ee16717f32413
Kashyap <kashyapshahare03@gmail.com>
2021-10-05 11:47:22 +0530

    C++ Code

---
+++ 2.6 Goto Statement in C++/goto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "One" << endl;
+    goto a;
+    cout << "Two" << endl;
+    cout << "Three" << endl;
+    a:
+    cout << "Four" << endl;
+}
+
+    // Goto function just jump to were to want to go 
+
+    // Simply it just skip the line b/w goto function.

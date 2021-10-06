854facf9482aa1f0773fd0a321bbcf222ac7a37e
Kashyap <kashyapshahare03@gmail.com>
2021-10-06 12:02:52 +0530

    C++ Code

---
+++ 3.2 Passing One Dimensional Array in Function in C++/array1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// It just tell us what sre the elements inside the array.
+
+void fun(int arr[3], int size) // You can also skip that 3 in array.
+{
+    for (int i=0; i<size; i++)
+    cout << arr[i] << endl;
+}
+
+int main(void)
+{
+    int arr[] = {1,2,6,4,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    fun(arr, size);
+}
+
+// sizeof(arr) give us the array size which is 5*4=20 and sizeof(arr[0]) gives us the size of 0th element in array.
+
+// so, sizeof(arr)/sixeof(arr[0]) = No. of element is in the array.
---
+++ 3.2 Passing One Dimensional Array in Function in C++/array2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// It just tell us what sre the elements inside the array.
+
+void fun(int *arr, int size) // Keeping pointer also works same
+{
+    for (int i=0; i<size; i++)
+    cout << arr[i] << endl;
+}
+
+int main(void)
+{
+    int arr[] = {1,2,6,4,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    fun(arr, size);
+}
+
+// sizeof(arr) give us the array size which is 5*4=20 and sizeof(arr[0]) gives us the size of 0th element in array.
+
+// so, sizeof(arr)/sixeof(arr[0]) = No. of element is in the array.
